99b5c988fb221d00af1aaed2e8aee48f46f25b67
Irene Ku Iveling <82irekui@skola.boras.se>
2020-10-06 13:59:27 +0200

    Lade till switch

--- Program.cs
+++ Program.cs
@@ -7,24 +7,64 @@ namespace TodoApp
     {
         static void Main()
         {
+            while(true)
+            {
+            // Console.Clear();
+            Console.WriteLine("-------Min Todo-App--------");
+            Console.WriteLine("--Välj vad du vill göra genom att välja bokstav--");
+            Console.WriteLine("[L]ägga till Todo");
+            Console.WriteLine("[V]ad har jag att göra i Todo-lista");
+            Console.WriteLine("[M]arkera Todo som klar");
+            Console.WriteLine("[T]a bort en Todo");
+            Console.WriteLine("[A]vsluta Todo");
+            Console.WriteLine("---------------------------");
+
+            
             TodoManager todoManager = new TodoManager();
 
-            //Test-code, add user interface here later
-            Console.WriteLine("Skriv in i Todo-lista:");
-            todoManager.AddTodoItem(Console.ReadLine());
-            todoManager.AddTodoItem("Rasta hunden");
-            todoManager.AddTodoItem("Rasta den andra hunden");
-            todoManager.AddTodoItem("Rasta papegojan");
+            string input = Console.ReadLine().ToUpper();
+            switch (input)
+            {
+                case "L":
+                    Console.WriteLine("Skriv in i Todo-lista:");
+                    todoManager.AddTodoItem(Console.ReadLine());
+                    
+                //Test-code
+                    todoManager.AddTodoItem("Rasta hunden");
+                    todoManager.AddTodoItem("Rasta den andra hunden");
+                    todoManager.AddTodoItem("Rasta papegojan");
+                    
+                    WriteListToConsole(todoManager.ListTodoItems());
+                    break;
 
-            // WriteListToConsole(todoManager.ListTodoItems());
-            //todoManager.ListOfTodoItems[1].CompleteTodo();
-            WriteListToConsole(todoManager.ListTodoItems());
-            Console.WriteLine("Vilken Todo är du klar med?");
-            todoManager.CompleteTodo(Convert.ToInt32(Console.ReadLine()));
+                case "V":
+                    WriteListToConsole(todoManager.ListTodoItems());
+                    break;
+                    
+                case "M":
+                    //todoManager.ListOfTodoItems[1].CompleteTodo();
+                    WriteListToConsole(todoManager.ListTodoItems());
+                    Console.WriteLine("Vilken Todo är du klar med?");
+                    todoManager.CompleteTodo(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                case "T":
+                    Console.WriteLine("Vilken Todo vill du ta bort?");
+                    todoManager.RemoveTodoIitem(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                    
+                case "A":
+                    Console.WriteLine("Avslutar Todo-app");
+                    return;
+                    
+                default:
+                    Console.WriteLine("Invalid input...");
+                    break;
+            }
+            }
+
+
             
-            Console.WriteLine("Vilken Todo vill du ta bort?");
-            todoManager.RemoveTodoIitem(Convert.ToInt32(Console.ReadLine()));
-            WriteListToConsole(todoManager.ListTodoItems());
             // todoManager.SetTodoItem(0, true);
             // todoManager.RemoveTodoItem(1);
         }
@@ -36,5 +76,6 @@ namespace TodoApp
                 Console.WriteLine((i+1) + " " + listString[i]);
             }
         }
+        
     }
 }
